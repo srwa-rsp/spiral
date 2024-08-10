d14467bb0389a6b54f53a04cef7884757934d9c6
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-08-10 11:57:37 -0400

    add users table

---
+++ migrations/20240810152151_user_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up (knex) {
+    await knex.schema.createTable('users', (table) => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('password').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+    await knex.schema.dropTable('users');
+  }
