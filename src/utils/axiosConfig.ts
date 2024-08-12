5e39bcdc1fc26fbefdc38161c9076801c19a5001
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-08-12 18:41:59 -0400

    add axios config

---
+++ src/utils/axiosConfig.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
