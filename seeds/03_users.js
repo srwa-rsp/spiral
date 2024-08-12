b44dee612f5df59605cfcad747be7b6ec3305a27
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-08-12 18:38:43 -0400

    add users seeds

---
+++ seeds/03_users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+import bcrypt from 'bcrypt' 
+export async function seed(knex) {
+    await knex('users').del();
+    const hashedPassword = await bcrypt.hash('123456', 10);
+    await knex('users').insert([
+        
+            {
+              id: 1,
+              name: "serwa",
+              email:"serwa@gmail.com",
+              password: hashedPassword,
+            },
+
+          
+
+    ]);
+  };
