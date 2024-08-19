f3a04221e53ea30178b65ca89a3778e8dc7344d8
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-08-19 19:13:43 -0400

    add background image

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,9 @@
 import Header from "@/components/Header/Header";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import spiralV from '@/assets/images/Spiral-Vertical.svg'
+import spiral from '@/assets/images/Big-Spiral.svg'
 
 interface Props {
   children?: ReactNode;
@@ -10,10 +13,12 @@ export const AuthLayout = ({ children }: Props) => {
   const router = useRouter();
 
   return (
-    <div className=" h-[100dvh]  flex flex-col bg-white md:bg-[url('/login.png')] overflow-y-hidden ">
+    <div className=" h-[100dvh]  flex flex-col bg-white relative">
       <Header />
-      <div className="flex basis-1/2 flex-col gap-6 w-full p-8 lg:p-16 justify-center">
+      <div className="flex flex-col gap-6 w-full p-8 lg:p-16 justify-center">
         {children}
+        <Image width={300} src={spiralV} alt={""} className="absolute bottom-0 right-10 " />
+        <Image width={300} src={spiral} alt={""} className="absolute top-0 left-0" />
       </div>
     </div>
   );
